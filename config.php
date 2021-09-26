efeee2e851b846d22c6db5c5ec96b3b5f235e49a
José Angel Espinosa Estrada <81893219+0wen30@users.noreply.github.com>
2021-09-26 15:54:33 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-    const SERVER = "fdb31.batcave.net";
-    const DB = "3909472_bd";
-    const USER = "3909472_bd";
-    const PASS = "zxcvbnm123+-";
+    const SERVER = "localhost";
+    const DB = "base1";
+    const USER = "root";
+    const PASS = "";
     const UTF8 = "utf8";
     const SGBD = "mysql:host=".SERVER.";dbname=".DB.";charset=".UTF8;
 ?>
